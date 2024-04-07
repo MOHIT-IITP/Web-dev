40fb445fb6147a20b9c27acdf89516454682a074
MOHIT-IITP <codingbymohit@gmail.com>
2024-04-07 21:05:22 +0530

    add mountain parallax

--- html_project/mountain parallax/js/script.js
+++ html_project/mountain parallax/js/script.js
@@ -1,27 +1,30 @@
 gsap.registerPlugin(ScrollTrigger);
-
+gsap.from(".main h1",{
+    y:-300,
+    opacity:0,
+})
 var tl = gsap.timeline({scrollTrigger:{
     trigger:".main2",
     start:"-50% 50%",
-    end:"30% 50%",
+    end:"40% 50%",
     scrub:true,
 }})
 
 tl.to("#leaf1",{
-    x:-300,
+    x:-400,
 },"leaf")
 
 tl.to("#leaf2",{
-    x:300,
+    x:400,
 },"leaf")
 
 tl.to("#bush1",{
-    y:200,
+    y:-300,
+},"leaf")
+tl.to("#bar",{
+    y:-300,
 },"leaf")
 
-tl.to("#bush2",{
-    y:300,
-},"leaf")
 tl.to(".main h1",{
     y:400,
     scale:1.5,
@@ -31,8 +34,8 @@ tl.to("#sun",{
 },"leaf")
 
 tl.to("#mountain1",{
-    y:-50,
+    y:100,
 })
 tl.to("#mountain2",{
-    y:-100,
+    y:200,
 })
