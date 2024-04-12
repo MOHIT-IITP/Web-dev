2fa35e51cf7d19bfdbac3f6dee6565492f42d807
MOHIT-IITP <codingbymohit@gmail.com>
2024-04-12 23:04:58 +0530

    update smooth scrolling effect js

--- html_project/smooth-scroll/script.js
+++ html_project/smooth-scroll/script.js
@@ -12,17 +12,22 @@ function raf(time) {
 requestAnimationFrame(raf)
 
 gsap.registerPlugin(ScrollTrigger);
-let tl = gsap.timeline({
+
+var tl = gsap.timeline({
     scrollTrigger:{
         trigger: ".page2",
         start:"10% 50%",
-        end: "60% 50%",
-        // markers: true,
+        end: "50% 50%",
+        markers: true,
         scrub:1,
     },
 })
     
-tl.to(".text1",{
+tl.to(".text1 h1",{
+    width:"100%",
+}),
+tl.to(".text1 h2",{
     width:"100%",
 })
+
     
