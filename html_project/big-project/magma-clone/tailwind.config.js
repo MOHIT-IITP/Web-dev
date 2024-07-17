c1a551381ca56e8e36d5483f6569a35d3f0828ac
MOHIT-IITP <codingbymohit@gmail.com>
2024-07-17 12:06:47 +0530

    add big project magma-clone

---
+++ html_project/big-project/magma-clone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'nila':'#083FDC',
+      }
+    },
+  },
+  plugins: [],
+}
+
