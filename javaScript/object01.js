93860cef5484cf706590138a4f77dbf5c1c3cc2d
MOHIT-IITP <codingbymohit@gmail.com>
2024-09-11 17:13:46 +0530

    done object

---
+++ javaScript/object01.js
@@ -0,0 +1,41 @@
+console.log("Learning Object Today")
+
+// singleton 
+// Object.create       using constructor
+
+const mySym = Symbol("key1")
+
+const user = {
+  name : "mohit",
+  "full name" : "mohit kumar",
+  age : 23,
+  [mySym] : "key12",
+  college : "IITP",
+  email : "mohit@google.com"
+}
+
+// two ways to get the keys and value of object
+
+console.log(user.email);
+console.log(user["email"]);
+console.log(user["full name"]);  // important
+console.log(user[mySym]);   // important
+
+
+
+const user1 = {
+  name : "golden",
+  "full name" : "mohit kumar",
+  age : 23,
+  [mySym] : "key12",
+  college : "IITP",
+  email : "mohit@chatgpt.com"
+}
+
+user1.email = "mohit@microsoft.com";
+Object.freeze(user1);
+// from now no change will be add to the main object
+
+user1.email = "golden@iitp.com";
+console.log(user1.email);  // mohit@microsoft.com
+
