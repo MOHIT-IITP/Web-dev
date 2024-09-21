38e33f48b0100a3bba562ceb6760ccef4e3914ac
MOHIT-IITP <codingbymohit@gmail.com>
2024-09-21 16:29:43 +0530

    done one async js project

--- javaScript/Dom/async_project/pro1/script.js
+++ javaScript/Dom/async_project/pro1/script.js
@@ -1,1 +1,27 @@
-document.querySelector('head');
+const startbtn = document.querySelector("#start");
+const stopbtn = document.querySelector("#stop");
+
+// making the function to say the name
+const sayMyName = function () {
+  console.log("MOHIITP");
+  console.log("IITP");
+};
+
+// making the setinterval with interval of 1 sec;
+let startInter;
+
+// adding the event to the start button
+startbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (!startInter) {
+    startInter = setInterval(sayMyName, 1000);
+  }
+});
+
+// adding the even to stop button
+stopbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  clearInterval(startInter);
+  startInter = null;
+});
