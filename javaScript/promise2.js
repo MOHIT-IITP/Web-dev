1c5e413b054f5e28e860f67ae19c097431875d3a
MOHIT-IITP <codingbymohit@gmail.com>
2024-10-02 08:36:59 +0530

    done promise 2

--- javaScript/promise2.js
+++ javaScript/promise2.js
@@ -24,3 +24,30 @@ async function consumepromisefive() {
   }
 }
 consumepromisefive();
+
+// take time and read the below code carefully
+async function getALLUsers() {
+  {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users",
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+getALLUsers();
+
+// writing the above code with then and catch
+
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(function (response) {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
