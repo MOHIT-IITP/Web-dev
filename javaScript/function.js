44ba2f6a03497368130d4af062072ac7a6932527
MOHIT-IITP <codingbymohit@gmail.com>
2024-10-07 19:56:54 +0530

    done updating the function

--- javaScript/function.js
+++ javaScript/function.js
@@ -1,53 +1,45 @@
 // explicit function
-function mynew( a , b ) {
-  return a+b;
-}
-console.log(mynew(23,34));
-
-
-const Lnew = function ( a, b) {
+function mynew(a, b) {
   return a + b;
 }
-Lnew(23,4); // this will not print the value in the terminal(console)
-console.log(Lnew(23,43));
+console.log(mynew(23, 34));
 
+const Lnew = function (a, b) {
+  return a + b;
+};
+Lnew(23, 4); // this will not print the value in the terminal(console)
+console.log(Lnew(23, 43));
 
-// implicit function 
-  
+// implicit function
+
 // const Onew = (first , second) => first + second ;
 
-const Onew = (first , second) => (first + second) ;
+const Onew = (first, second) => first + second;
+const Onew = (first, second) => console.log(first + second);
 
+console.log(Onew(2, 3));
 
-console.log(Onew(2,3));
-
-// using $this 
+// using $this
 const newfun = () => {
   fullname = "mohit";
   welcome = () => {
     console.log(`${this.fullname}`);
-  }
+  };
   welcome();
-}
+};
 
 console.log(newfun());
 
+// hoisting
+newfunc1(); // calling before creating this
 
-
-
-// hoisting 
-newfunc1(); // calling before creating this 
-
-function newfunc1 () {
+function newfunc1() {
   console.log("this is function 1");
 }
 
-// cannot declare this function before creating this 
-nyc(); // this will give error 
+// cannot declare this function before creating this
+nyc(); // this will give error
 
-const nyc = function() {
+const nyc = function () {
   console.log("this is second fucntion");
-}
-
-
-
+};
---
+++ javaScript/note.md
@@ -0,0 +1,1 @@
+# JS and classes
